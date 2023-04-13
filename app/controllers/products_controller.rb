37a51ce1eb3df1868099969481c98024a5797ff5
Emily Port <emilylport@gmail.com>
2023-04-13 13:00:56 -0600

    rate limited by api key, sad path logic if user is nil or api key is invalid

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,20 +2,26 @@ class ProductsController < ApplicationController
   before_action :find_user
 
   def index 
-    # render json: ProductSerializer.new(ProductFacade.all_products)  
-    product_cache = Rails.cache.read(['products_data', @user])
-    if product_cache == nil
-      product_cache = ProductFacade.all_products
-      Rails.cache.write(['products_data', @user], product_cache, expires_in: 1.minute)
-      render json: ProductSerializer.new(product_cache)
-    else
-      render json: ProductSerializer.new(product_cache)
+    if @user != nil && params[:api_key]
+
+      # render json: ProductSerializer.new(ProductFacade.all_products)  
+      product_cache = Rails.cache.read(['products_data', @user])
+      if product_cache == nil
+        product_cache = ProductFacade.all_products
+        Rails.cache.write(['products_data', @user], product_cache, expires_in: 1.minute)
+        render json: ProductSerializer.new(product_cache)
+      else
+        render json: ProductSerializer.new(product_cache)
+      end
+    else 
+      render json: {error: "Please create an account to receive an API key"}
     end
   end
 
   private 
   def find_user
-    @user = current_user
+    @user = User.find_by(api_key: params[:api_key])
+    # @user = current_user
   end
 
 end
--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,8 +1,30 @@
 class Rack::Attack 
   Rack::Attack.cache.store = Rails.cache 
 
+  # def body_params
+  #   unless @body_params
+  #     @body_params = JSON.parse(body.read)
+  #     body.rewind
+  #   end
+  #   @body_params
+  # end
 
-  Rack::Attack.throttle('products_limit', limit: 2, period: 30.seconds) do |req|
-    req.ip if req.path.include?('/products') && req.get?
+  # def api_key
+  #   body_params["api_key"]
+  # end
+  Rack::Attack.throttle("api_key:limit-per-seconds", limit: 2, period: 30.seconds) do |req|
+    # require 'pry'; binding.pry
+    # body_params = JSON.parse(req.body.read)
+    # req.body.to_json
+    # require 'pry'; binding.pry
+    if req.path.include?('/products') && req.get?
+      # body_params["api_key"]
+      # require 'pry'; binding.pry
+      # JSON.parse(req.body.read)["api_key"]
+      req.params["api_key"] 
+    end
   end
+
+
+  # @params = JSON.parse(req.body.read)
 end
