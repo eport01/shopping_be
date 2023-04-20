fac41efc95d3fa356f70f49946e5f087d5a5558d
Emily Port <emilylport@gmail.com>
2023-04-20 14:43:22 -0600

    rack attack logic

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,9 +1,6 @@
 class Rack::Attack 
   Rack::Attack.cache.store = Rails.cache 
-  # require 'pry'; binding.pry
-  # def self.wday 
-  #   Time.now.wday 
-  # end
+
   if Time.now.wday.between?(1,5) 
     Rack::Attack.throttle("api_key:weekday-limit", limit: 2, period: 30.seconds) do |req|
       if req.path.include?('/products') && req.get?
